b8614783684beacc8e7183ca9c6fe721ef322b2c
bayzhen <1521085156@qq.com>
2020-09-15 14:58:33 +0800

    Update module.py

--- module.py
+++ module.py
@@ -2,3 +2,9 @@ import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
+
+#项目要开始进行一段时间的搁置了，我计划使用ANSYS现成的模块来完成有限元分析等操作
+#但ANSYS属于我没有接触过的软件
+#我曾经接触过Solidworks，虽然SW也能做有限元分析，但机械工业上大部分还是都在使用ANSYS
+#如果使用SW，这对于软件未来的发展并不好。
+
